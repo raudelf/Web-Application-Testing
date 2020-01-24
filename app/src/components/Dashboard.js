5c30fc77d3159c82baa2ecbe3b17f1362a0bbfd2
Raudel Flores <rawdell@outlook.com>
2020-01-24 11:31:10 -0600

    created the Dashboard component and moved over all the buttons into this file. Imported addCount and hitBtn to make it easier and incorporated props as well

---
+++ app/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { addCount, hitBtn } from '../App';
+
+const Dashboard = (props) => {
+    return (
+        <div className='buttons'>
+        <div className='bottomButtons'>
+          <button className='ballBtn' onClick={() => props.setBallCount(addCount(props.ballCount))}>Ball</button>
+          <button className='strikeBtn' onClick={() => props.setStrikeCount(addCount(props.strikeCount))}>Strike</button>
+          <button className='outBtn' onClick={() => props.setOutCount(addCount(props.outCount))}>Out</button>
+        </div>
+        <button className='hitBtn' onClick={() => hitBtn(props.setBallCount, props.setStrikeCount)}>Hit</button>
+      </div>
+    )
+}
