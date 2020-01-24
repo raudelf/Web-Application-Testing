2c783814ca7f54ce2ba6cbd274fba6803e2dcbc7
Raudel Flores <rawdell@outlook.com>
2020-01-23 22:55:19 -0600

    rearranged the it test

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App, { addCount } from './App';
-import { unmountComponentAtNode } from 'react-dom';
+import ReactDOM, { unmountComponentAtNode } from 'react-dom';
 
 it('Renders without errors!', () => {
+  // ARRANGE
   const div = document.createElement("div");
 
-  render(<App />, div);
+  // ACT
+  ReactDOM.render(<App />, div);
 
-  unmountComponentAtNode(div);
+  // CLEAN UP
+  ReactDOM.unmountComponentAtNode(div);
+
+  // ASSERTION - Rendered without exception
+});
+
+test('RTL Renders without Crashing', () => {
+  render(<App />);
 });
 
 test('Function addCount has no errors!', () => {
