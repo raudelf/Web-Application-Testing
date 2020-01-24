4f00d339664b6a9a84cb6047ac9651ebda93eb2c
Raudel Flores <rawdell@outlook.com>
2020-01-24 11:06:31 -0600

    deleted the display code and imported it from a different component

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import Display from './components/Display';
 
 export const addCount = initialValue => {
   return initialValue + 1
@@ -35,36 +36,7 @@ function App() {
       <header className="header">
         <h1>Scoreboard App</h1>
       </header>
-      <div className='scoreboard'>
-        <div className='topRow'>
-          <div className='home Box'>
-            <h1 className='homeName'>HOME</h1>
-            <div className='home Count'>{homeScore}</div>
-          </div>
-          <div className='inning Box'>
-            <h1>INNING</h1>
-            <div className='inning Count'>{inningCount}</div>
-          </div>
-          <div className='away Box'>
-            <h1 className='awayName'>AWAY</h1>
-            <div className='away Count'>{awayScore}</div>
-          </div>
-        </div>
-        <div className='bottomRow'>
-          <div className='ballCount Box'>
-            <h1 className='ballName'>BALL</h1>
-            <div className='ball Count'>{ballCount}</div>
-          </div>
-          <div className='strike Box'>
-            <h1 className='strikeName'>STRIKE</h1>
-            <div className='strike Count'>{strikeCount}</div>
-          </div>
-          <div className='out Box'>
-            <h1 className='outName'>OUT</h1>
-            <div className='out Count'>{outCount}</div>
-          </div>
-        </div>
-      </div>
+      <Display homeScore={homeScore} inningCount={inningCount} awayScore={awayScore} ballCount={ballCount} strikeCount={strikeCount} outCount={outCount}/>
       <div className='buttons'>
         <div className='bottomButtons'>
           <button className='ballBtn' onClick={() => setBallCount(addCount(ballCount))}>Ball</button>
